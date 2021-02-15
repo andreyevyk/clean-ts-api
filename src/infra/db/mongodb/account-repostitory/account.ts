39c36b89e33326e2ca404cb7472f1976219a8ed2
Andrey Evyk <andreyevyk13@gmail.com>
2021-02-15 00:23:13 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/account-repostitory/account.ts
+++ src/infra/db/mongodb/account-repostitory/account.ts
@@ -2,14 +2,10 @@ import { AddAccountRepo } from '../../../../data/protocols/add-account-repo'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/useCases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
-
 export class AccountMongoRepo implements AddAccountRepo {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
-
-    const account = result.ops[0]
-    const { _id, ...accountWithoutId } = account
-    return Object.assign({}, accountWithoutId, { id: _id })
+    return MongoHelper.map(result.ops[0])
   }
 }
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -16,5 +16,11 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectiontWithoutId } = collection
+    return Object.assign({}, collectiontWithoutId, { id: _id })
   }
+
 }
