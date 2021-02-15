ce43b4e5deb273545bbc82e8d3660ef7c52bed8c
Andrey Evyk <andreyevyk13@gmail.com>
2021-02-15 15:42:56 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,4 @@
-import express from 'express'
-
-const app = express()
-
-app.use(express.json())
+import app from './config/app'
 
 app.listen(5050, () => {
   console.log('Server running at http://localhost:5050')
